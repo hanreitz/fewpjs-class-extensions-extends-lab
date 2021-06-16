7a20894796143f996c5198641c156e1cc93a2af8
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-06-16 15:29:30 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+  constructor (sideArray) {
+    this.sideArray = sideArray
+  }
+
+  get countSides () {
+    return this.sideArray.length
+  }
+
+  get perimeter () {
+    return this.sideArray.reduce((memo, e) => memo + e)
+  }
+
+}
+
+class Triangle extends Polygon {
+  get isValid () {
+    const truthCheck = (this.sideArray[0] < this.sideArray[1] + this.sideArray[2]) && 
+      (this.sideArray[1] < this.sideArray[0] + this.sideArray[2]) && 
+      (this.sideArray[2] < this.sideArray[0] + this.sideArray[1])
+    return !!truthCheck
+  }
+}
+
+class Square extends Polygon {
+  get isValid () {
+    const sides = [...new Set(this.sideArray)]
+    return (!!(sides.length === 1))
+  }
+
+  get area () {
+    return (this.sideArray[0]**2)
+  }
+
+}
